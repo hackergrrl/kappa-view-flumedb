e284f93c734c3905949298441d2cc1b2cddc44c8
noffle <sww@eight45.net>
2019-07-05 10:15:45 -0700

    fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var View = require('kappa-view')
 
-module.exports = function (log, idx) {
-  var view = View(idx, function (lvl) {
+module.exports = function (idx, log) {
+  return View(idx, function (lvl) {
     return {
       map: function (entries, next) {
         console.log('map', entries)
-        lvl.append(entries, next)
+        log.append(entries, next)
       }
     }
   })
